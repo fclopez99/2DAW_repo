5d25892bdee999bfc54d69df72eef4494aa893ce
FabiánCaamaño <fabiancl1999@gmail.com>
2022-09-13 14:06:08 +0200

    commit con operadores aritméticos

---
+++ JS/operadores.js
@@ -0,0 +1,11 @@
+var num1 = prompt ("Introduce el primer valor: ");
+var num2 = prompt ("Introduce el segundo valor: ");
+    //var suma = (num1 + num2);
+    var resta = num1 - num2;
+    var producto = num1 * num2;
+    var división = num1 / num2;
+    var módulo = num1 % num2;
+    var potencia = num1 ** num2;
+
+alert("Resultados: " + "\nSuma: " + (parseInt(num1)+parseInt(num2)) + "\nResta: " + resta + "\nProducto: " + producto + "\nDivisión: " + división+ "\nMódulo: " + módulo+"\nPotencia: "+potencia);
+
--- JS/variable.js
+++ JS/variable.js
@@ -1,8 +1,12 @@
 /*Script para incrementar un número almacenado nunha variable
 e imprimir por pantalla*/
-
+/*
 var num = 9;
 if (num == 9) {
     num = num + 1;
     document.write(num);
-}
+}*/
+let nombre = prompt ("Introduce el nombre y apellidos: ");
+            //document.write("El nombre introducido es: " + nombre);
+            let direccion = prompt ("Introduce la dirección: ");
+            alert("Datos introducidos: " + "\nNombre y apellidos: " + nombre + "\nDirección: " + direccion);
