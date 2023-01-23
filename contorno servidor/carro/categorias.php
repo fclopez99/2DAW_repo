a7870b10816fb097f8c7cf8877c0f7f99431a8fe
FabiánCaamaño <fabiancl1999@gmail.com>
2023-01-23 10:23:55 +0100

    aplicacion carro da compra contorno servidor

--- contorno servidor/carro/categorias.php
+++ contorno servidor/carro/categorias.php
@@ -13,13 +13,13 @@
         echo "<a href='categorias.php'>Home</a> <a href='carrito.php'>Ver Carrito</a> <a href='Vaciar_Carro.php'>Vaciar Carro</a> <a href='Rex_Categorias.php'>Rexistro Categorias</a> <a href='Rex_Productos.php'>Rexistrar Productos</a> <a href='Rex_Users.php'>Rexistrar Usuarios</a> <a href='logout.php'>Cerrar sesión</a><hr>";
         
         if($con){
-            $sql = "select codCat, nombre from categorias order by codCat";
+            $sql = "select CodCat, nombre from categorias order by CodCat";
             $consulta = mysqli_query($con, $sql);
             if($consulta){
                 $fila = $consulta->fetch_assoc();
                 echo "<h2>Lista de Categorías</h2>";
                 while($fila){
-                    echo "<li>"."<a href='productos.php/categoria=".$fila["codCat"]."'>".$fila["nombre"]."</a></li>";
+                    echo "<li>"."<a href='productos.php/categoria=".$fila["CodCat"]."'>".$fila["nombre"]."</a></li>";
                     $fila = $consulta->fetch_assoc();
                 }
             }
